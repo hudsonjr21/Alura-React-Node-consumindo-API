49b12882dcf6ff821a740a94ed2849647852efb0
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-03 14:54:19 -0300

    Parte 1 - Finalizado

---
+++ Alura-book-front/src/components/OptionsHeader/index.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Opcoes = styled.ul`
+  display: flex;  
+`
+
+const Opcao = styled.li`
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'ESTANTE'];
+
+function OptionsHeader() {
+    return (
+        <Opcoes>
+          { textoOpcoes.map ( (texto) => (
+            <Link to={`/${texto.toLowerCase()}`} >
+              <Opcao>
+                <p>{texto}</p>
+              </Opcao>
+            </Link>
+          ))}
+        </Opcoes>
+    )
+}
+
+export default OptionsHeader;
