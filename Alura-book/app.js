665f1565aca0f72b1352ce0ecc9fcb0c39e3a3fe
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-08 13:44:33 -0300

    Parte 3 - Finalizado

--- Alura-book/app.js
+++ Alura-book/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const rotaLivro = require("./routes/livro");
+const rotaFavorito = require("./routes/favorito")
+
 const cors = require("cors")
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "*"}))
+app.use(cors({origin: "*"}));
 
 app.use('/livros', rotaLivro);
+app.use('/favoritos', rotaFavorito);
 
 const port = 8000
 
